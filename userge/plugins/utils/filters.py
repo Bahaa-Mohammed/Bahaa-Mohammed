1b499ba137c850621029384576001c0e9a2f2495
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2021-06-18 00:27:37 +0300

    Update filters.py

--- userge/plugins/utils/filters.py
+++ userge/plugins/utils/filters.py
@@ -192,6 +192,7 @@ async def chat_filter(message: Message) -> None:
                                              message_id=FILTERS_DATA[message.chat.id][name],
                                              chat_id=message.chat.id,
                                              user_id=message.from_user.id,
-                                             reply_to_message_id=message.message_id)
+                                             reply_to_message_id=message.message_id,
+disable_web_page_preview=false)
     except RuntimeError:
         pass
